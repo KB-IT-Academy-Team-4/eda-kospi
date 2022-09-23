5f63562bc019593226afc4e3df8c4d4b6f53ce2d
Byeong Heon Lee <lww7438@gmail.com>
2022-09-23 12:10:29 +0900

    UPDATE apiConfig.py

--- apiConfig.py
+++ apiConfig.py
@@ -35,6 +35,26 @@ URL_KOSPI_SERIES_DAILY_PRICE           = "http://data-dbg.krx.co.kr/svc/apis/idx
 URL_KOSDAQ_SERIES_DAILY_PRICE          = "http://data-dbg.krx.co.kr/svc/apis/idx/kosdaq_dd_trd"                                   # KOSDAQ 시리즈 일별시세정보
 URL_BOND_INDEX_DAILY_PRICE             = "http://data-dbg.krx.co.kr/svc/apis/idx/bon_dd_trd"                                      # 채권지수 시세정보
 URL_DERIVATIVE_INDEX_DAILY_PRICE       = "http://data-dbg.krx.co.kr/svc/apis/idx/drvprod_dd_trd"                                  # 파생상품지수 시세정보
+URL_KOSPI_DAILY_TRADING                = "http://data-dbg.krx.co.kr/svc/apis/sto/stk_bydd_trd"                                    # 유가증권 일별매매정보
+URL_KOSDAQ_DAILY_TRADING               = "http://data-dbg.krx.co.kr/svc/apis/sto/ksq_bydd_trd"                                    # 코스닥 일별매매정보
+URL_KONEX_DAILY_TRADING                = "http://data-dbg.krx.co.kr/svc/apis/sto/knx_bydd_trd"                                    # 코넥스 일별매매정보
+URL_SW_DAILY_TRADING                   = "http://data-dbg.krx.co.kr/svc/apis/sto/sw_bydd_trd"                                     # 신주인수권증권 일별매매정보
+URL_SR_DAILY_TRADING                   = "http://data-dbg.krx.co.kr/svc/apis/sto/sr_bydd_trd"                                     # 신주인수권증서 일별매매정보
+URL_KOSPI_BASE_INFO                    = "http://data-dbg.krx.co.kr/svc/apis/sto/stk_isu_base_info"                               # 유가증권 종목기본정보
+URL_KOSDAQ_BASE_INFO                   = "http://data-dbg.krx.co.kr/svc/apis/sto/ksq_isu_base_info"                               # 코스닥 종목기본정보
+URL_KONEX_BASE_INFO                    = "http://data-dbg.krx.co.kr/svc/apis/sto/knx_isu_base_info"                               # 코넥스 종목기본정보
+URL_ETF_DAILY_TRADING                  = "http://data-dbg.krx.co.kr/svc/apis/etp/etf_bydd_trd"                                    # ETF 일별매매정보
+URL_ETN_DAILY_TRADING                  = "http://data-dbg.krx.co.kr/svc/apis/etp/etn_bydd_trd"                                    # ETN 일별매매정보
+URL_ELW_DAILY_TRADING                  = "http://data-dbg.krx.co.kr/svc/apis/etp/elw_bydd_trd"                                    # ELW 일별매매정보
+URL_GOV_BOND_DAILY_TRADING             = "http://data-dbg.krx.co.kr/svc/apis/bon/kts_bydd_trd"                                    # 국채전문유통시장 일별매정보
+URL_GENERAL_BOND_DAILY_TRADING         = "http://data-dbg.krx.co.kr/svc/apis/bon/kts_bydd_trd"                                    # 일반채권시장 일별매매정보
+URL_SMALL_BOND_DAILY_TRADING           = "http://data-dbg.krx.co.kr/svc/apis/bon/smb_bydd_trd"                                    # 소액채권시장 일별매매정보
+# 선물 일별매매정보 (주식선물 외)
+# 주식선물(유가) 일별매매정보
+# 주식선물(코스닥) 일별매매정보
+# 옵션 일별매매정보 (주식옵션 외)
+# 주식옵션(유가) 일별매매정보
+# 주식옵션(코스닥) 일별매매정보
 URL_PETROLEUM_MARKET_DAILY_TRADING     = "http://data-dbg.krx.co.kr/svc/apis/gen/oil_bydd_trd"                                    # 석유시장 일별매매정보
 URL_GOLD_MARKET_DAILY_TRADING          = "http://data-dbg.krx.co.kr/svc/apis/gen/gold_bydd_trd"                                   # 금시장 일별매매정보
 URL_CARBON_CREDIT_MARKET_DAILY_TRADING = "http://data-dbg.krx.co.kr/svc/apis/gen/ets_bydd_trd"                                    # 배출권 시장 일별매매정보
@@ -43,7 +63,7 @@ URL_CARBON_CREDIT_MARKET_DAILY_TRADING = "http://data-dbg.krx.co.kr/svc/apis/gen
 # The number of Maximum Items in Korea Stock Exchange (KOSPI/KOSDAQ/KONEX)
 # http://data.krx.co.kr/contents/MDC/MAIN/main/index.cmd
 KOSPI_ITEMS   = 939
-KOSDAQ_ITEMS  = 1581
+KOSDAQ_ITEMS  = 1582
 KONEX_ITEMS   = 125
 ALL_STOCKS_KR = KOSPI_ITEMS + KOSDAQ_ITEMS + KONEX_ITEMS
 
